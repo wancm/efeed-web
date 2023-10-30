42f2e1eab5fdd274005ec335c9c7e50e16f32ce4
1cmstudio <wanchangmeng@gmail.com>
2023-10-30 13:46:15 +0800

    globamantic training files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,5 @@
-import { cacheService } from "@/libs/shared/cache/cache-service";
+import App from "./components/app";
 
-let sessionDisplay = "this is session";
-let counter = 1;
+const Index = () => <App />;
 
-sessionDisplay = `${sessionDisplay} ${counter}`;
-
-const sessionStart = async () => {
-  cacheService.trySetAsync(
-    "session",
-    {
-      name: "this is session",
-    },
-    10
-  );
-};
-
-const sessionPooling = async () => {
-  const session = await cacheService.tryGetAsync("session");
-  counter++;
-
-  if (session) {
-    sessionDisplay = `${sessionDisplay} ${counter}`;
-
-    setTimeout(async () => {
-      await sessionPooling();
-    }, 1000);
-  } else {
-    sessionDisplay = `session expired`;
-  }
-};
-
-export default async function Home() {
-  const session = await sessionStart();
-
-  setTimeout(async () => {
-    await sessionPooling();
-  }, 1000);
-
-  return <h1>{`${sessionDisplay}`}</h1>;
-}
+export default Index;
